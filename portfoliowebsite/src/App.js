ae71fa79f436f09ed0c1f749f8ebb5520410a3ed
Rachel Long <rachellong@Rachels-MacBook-Pro.local>
2020-02-08 22:04:51 -0500

    outline

---
+++ portfoliowebsite/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <span>About</span>
+          <span>Contact</span>
+        </nav>
+        <div>
+        <p>Hi, my name is</p>
+        <h1> Rachel Long</h1>
+        <p>Im a Full Stack Web Developer 
+based in Richmond, Va specializing 
+In front end websites and applications</p>
+        <a>
+          Get in touch
+        </a>
+        </div>
+        <div>
+          <img />
+        </div>
+      </header>
+      <main>
+        <h2>Portfolio</h2>
+        <h2>About</h2>
+        <h2>Contact</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
