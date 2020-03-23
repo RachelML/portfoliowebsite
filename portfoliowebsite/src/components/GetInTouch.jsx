0a448ce8fe5fc63415b905155f98fca8b315d0d6
Rachel Long <rachellong@Rachels-MacBook-Pro.local>
2020-03-22 21:47:28 -0400

    styling

--- portfoliowebsite/src/components/GetInTouch.jsx
+++ portfoliowebsite/src/components/GetInTouch.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import { Route, Redirect } from 'react-router-dom';
 import { HashLink } from "react-router-hash-link";
+import PDF from '../documents/Rachel-Long.pdf'
 
 
 
@@ -15,7 +16,8 @@ export default class GetInTouch extends Component {
    
         <div className="contact-links">
             <p className="in-touch"> <a className="getin" style={{textDecoration: 'none'}} href='mailto:rachelricelong@gmail.com'>GET IN TOUCH</a></p>    
-            <p className="resume">RESUME</p>
+            <p className="resume"><a className="resume-link"  style={{textDecoration: 'none', color: 'white'}} href = {PDF}>RESUME</a>
+</p>
         </div>
         )
     }
