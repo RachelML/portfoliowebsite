683fc67bbe6c4431b5a1c3b76160a3a59f8c650e
Rachel <Rachelricelong@gmail.com>
2021-02-10 11:12:33 -0500

    multicolor name update

--- portfoliowebsite/src/App.js
+++ portfoliowebsite/src/App.js
@@ -12,13 +12,46 @@ import GetInTouch from './components/GetInTouch';
 
 
 function App() {
+//   componentDidMount() {
+//     const hero = document.querySelector(".hero");
+//     this.width = hero.offsetWidth;
+//     this.height = hero.offsetHeight;
+
+//     this.text = hero.querySelector("h1");
+//     this.walk = 20;
+//   }
+
+//   _onMouseMove(e) {
+//     const xWalk = Math.round(
+//       (e.screenX / this.width) * this.walk - this.walk / 2
+//     );
+//     const yWalk = Math.round(
+//       (e.screenY / this.height) * this.walk - this.walk / 2
+//     );
+
+//     this.text.style.textShadow = `
+//       ${xWalk}px ${yWalk}px 0px rgba(255,0,255,0.7),
+//       ${xWalk * -1}px ${yWalk}px 0px rgba(0,255,255,0.7),
+//       ${yWalk}px ${xWalk * -1}px 0px rgba(0,255,0,0.7),
+//       ${yWalk * -1}px ${xWalk}px 0px rgba(0,0,255,0.7)
+//     `;
+//     this.render();
+
+// }
   return (
     <div className="app">
       <Nav />
 
         <header className="App-header">
          <div className='bio'>
-          <p>Hi, my name is<br></br> Rachel Long. <br></br>I'm a Full Stack Web Developer 
+          <p>Hi, my name is</p>
+          <p> Rachel Long. </p>
+          {/* <div className="hero" onMouseMove={this._onMouseMove.bind(this)}>
+                <h1 className="name">
+                RACHEL LONG        
+                 </h1>
+                </div>  */}
+          <p>I'm a Full Stack Web Developer 
                based in Richmond, Va specializing 
                in high-quality websites and applications.
           </p>
--- portfoliowebsite/src/components/Nav.jsx
+++ portfoliowebsite/src/components/Nav.jsx
@@ -5,42 +5,14 @@ import { HashLink } from "react-router-hash-link";
 
 
 export default class Nav extends Component {
-    // componentDidMount() {
-    //     const hero = document.querySelector(".hero");
-    //     this.width = hero.offsetWidth;
-    //     this.height = hero.offsetHeight;
-    
-    //     this.text = hero.querySelector("h1");
-    //     this.walk = 20;
-    //   }
-    
-    //   _onMouseMove(e) {
-    //     const xWalk = Math.round(
-    //       (e.screenX / this.width) * this.walk - this.walk / 2
-    //     );
-    //     const yWalk = Math.round(
-    //       (e.screenY / this.height) * this.walk - this.walk / 2
-    //     );
-    
-    //     this.text.style.textShadow = `
-    //       ${xWalk}px ${yWalk}px 0px rgba(255,0,255,0.7),
-    //       ${xWalk * -1}px ${yWalk}px 0px rgba(0,255,255,0.7),
-    //       ${yWalk}px ${xWalk * -1}px 0px rgba(0,255,0,0.7),
-    //       ${yWalk * -1}px ${xWalk}px 0px rgba(0,0,255,0.7)
-    //     `;
-    //     this.render();
-    // }
+   
 
     render() {
         return (
             <div>
                 <div className="nav-container">
                     
-                 {/* <div className="hero" onMouseMove={this._onMouseMove.bind(this)}>
-        <h1 className="name">
-          RACHEL LONG        
-        </h1>
-      </div>  */}
+               
                      <p className="nav-about">  <HashLink  style={{textDecoration: 'none'}} to="/#about">
                      ABOUT
                     </HashLink></p>
